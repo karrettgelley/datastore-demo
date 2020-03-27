aa2a7df2b9fe54d6651bbc906f491ac98f4cf130
Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>
2020-03-27 16:20:24 -0700

    Observe on useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataStore } from '@aws-amplify/datastore';
 import { User } from './models';
 
 function App() {
+  useState(() => {
+    const subscription = DataStore.observe().subscribe(console.log);
+
+    return () => subscription.unsubscribe();
+  }, [])
   const handleAddUser = async () => {
     try {
       const newUser = await DataStore.save(
